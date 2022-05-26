ae6e181be8db3c878ccd571e38c4a2086d85564a
unknown <Gabriel Miranda>
2022-05-26 14:48:21 -0300

    style da dash modificado

--- src/database/script.sql
+++ src/database/script.sql
@@ -19,3 +19,18 @@ create table albums (
 );
 
 -- no campo fk_album, pegar o valor da combo box, exemplo, value = 2.
+
+insert into albums 
+	values (null, 'please please me', '1963'),
+    (null, 'with the beatles', '1963'),
+    (null, 'a hard days night', '1964'),
+    (null, 'beatles for sale', '1964'),
+    (null, 'help!', '1965'),
+    (null, 'rubber soul', '1965'),
+	(null, 'revolver', '1965'),
+	(null, 'sgt.peppers ', '1965'),
+    (null, 'magical mystery tour', '1966'),
+    (null, 'the beatles - white album', '1966'),
+    (null, 'yellow submarine', '1966'),
+    (null, 'abbey road', '1967'),
+	(null, 'let it be', '1967');
--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -25,7 +25,6 @@ function buscarMedidasEmTempoReal(idAquario) {
     return database.executar(instrucaoSql);
 }
 
-
 module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal
