e8e0c20439def98fe7e38a6f19ff61b65a56bb2a
Gabriel Miranda <gabriel.romao@sptech.school>
2022-06-03 15:08:44 -0300

    backend do card e dash

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,28 +1,35 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario) {
-    instrucaoSql = `select 
-                        ab.nome_album,
-                        max(us.idade_usuario),
-                        count(us.id_usuario)
-                        from 
-                        usuarios as us
-                        join albums as ab
-                        on ab.id_album = us.fk_album group by us.fk_album`;
+    instrucaoSql =
+    `select 
+        nome_album as 'Nome album', 
+        count(*) as 'Votos' 
+        from 
+        usuarios join albums 
+        on id_album = fk_album 
+        group by id_album;`; 
+    // `select 
+    //                     ab.nome_album,
+    //                     max(us.idade_usuario),
+    //                     count(us.id_usuario)
+    //                     from 
+    //                     usuarios as us
+    //                     join albums as ab
+    //                     on ab.id_album = us.fk_album group by us.fk_album`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(id_usuario) {
-    instrucaoSql = `select 
-                        ab.nome_album,
-                        max(us.idade_usuario),
-                        count(us.id_usuario)
-                        from 
-                        usuarios as us
-                        join albums as ab
-                        on ab.id_album = us.fk_album group by us.fk_album`;
-
+    instrucaoSql = 
+    `select 
+        nome_album, 
+        count(*) as votos 
+        from 
+        usuarios join albums 
+        on id_album = fk_album 
+        group by id_album;`; 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
